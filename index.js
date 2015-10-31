2a483038e1a7982ee2f7523a3ba8e0fbef0bf96c
Brian Jones <bjones03@gmail.com>
2015-10-31 01:09:49 -0400

    Ignoring and Removing compiled assets from source control. Fixing path for www folder to be os specific. Fixing placeholder for zipCode.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var path = require('path');
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var sh = require('shelljs');
 
-var path = __dirname + '/www';
+var www_root = path.join(__dirname, 'www');
 var port = 8080;
 
-connect().use(serveStatic(path)).listen(port);
+connect().use(serveStatic(www_root)).listen(port);
 
-console.log('Serving directory ' + path + '. Server listening at localhost:' + port.toString());
+console.log('Serving directory ' + www_root + '. Server listening at localhost:' + port.toString());
